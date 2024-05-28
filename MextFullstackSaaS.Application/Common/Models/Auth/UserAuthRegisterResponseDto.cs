eb1ae29627fd884e8d3982429bccb5cbc08fcb8a
Beyza Hilal <beyzakaramert@gmail.com>
2024-05-28 10:06:49 +0300

    Add project files.

---
+++ MextFullstackSaaS.Application/Common/Models/Auth/UserAuthRegisterResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MextFullstackSaaS.Application.Common.Models.Auth;
+
+public class UserAuthRegisterResponseDto
+{
+    public Guid Id { get; set; }
+
+    public string Email { get; set; }
+
+    public UserAuthRegisterResponseDto(Guid id, string email)
+    {
+        Id = id;
+
+        Email = email;
+    }
+}
---
+++ MextFullstackSaaS.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using MextFullstackSaaS.Application.Common.Interfaces;
+using MextFullstackSaaS.Domain.Identity;
+using MextFullstackSaaS.Domain.Settings;
+using MextFullstackSaaS.Infrastructure.Persistence.Contexts;
+using MextFullstackSaaS.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace MextFullstackSaaS.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>(
+                container => container.GetRequiredService<ApplicationDbContext>());
+
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.Configure<JwtSettings>(jwtSettings => configuration.GetSection("JwtSettings").Bind(jwtSettings));
+
+            services.AddIdentity<User, Role>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IJwtService, JwtManager>();
+            services.AddScoped<IIdentityService, IdentityManager>();
+          
+            return services;
+        }
+    }
+}
