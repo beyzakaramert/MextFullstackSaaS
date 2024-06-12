0baaaab45dba96ce630639ed0878412a5858adcf
Beyza Hilal <beyzakaramert@gmail.com>
2024-06-12 15:02:40 +0300

    [Feature]add Authorization

--- MextFullStackSaaS.WasmClient/Program.cs
+++ MextFullStackSaaS.WasmClient/Program.cs
@@ -1,8 +1,13 @@
+using Blazored.LocalStorage;
 using Blazored.Toast;
+using MextFullstackSaaS.WasmClient;
 using MextFullStackSaaS.WasmClient;
 using MextFullStackSaaS.WasmClient.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,5 +20,20 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) 
 builder.Services.AddBlazoredToast();
 
 builder.Services.AddScoped<IToasterService, BlazoredToasterManager>();
+
+builder.Services.AddBlazoredLocalStorage(config =>
+{
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
