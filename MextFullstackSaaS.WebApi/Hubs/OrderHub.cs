6325ce4a147da1212377a56a140e7d14ee50dcac
Beyza Hilal <beyzakaramert@gmail.com>
2024-06-25 10:50:11 +0300

    [Feature]add SignalAR

---
+++ MextFullstackSaaS.WebApi/Hubs/OrderHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MextFullstackSaaS.WebApi.Hubs
+{
+    public class OrderHub : Hub
+    {
+        public async Task NewOrderAddedAsync()
+        {
+
+        }
+
+        public async Task GetAllOrderForCommunityAsync()
+        {
+
+        }
+    }
+}
