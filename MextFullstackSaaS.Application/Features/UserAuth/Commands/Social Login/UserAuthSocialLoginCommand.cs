4ac62ed4a4f94eb18b62fb58d36a5084aac56eb3
Beyza Hilal <beyzakaramert@gmail.com>
2024-06-28 09:26:01 +0300

    [Feature]add UserAuthSocialLoginCommand

---
+++ MextFullstackSaaS.Application/Features/UserAuth/Commands/Social Login/UserAuthSocialLoginCommand.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using MextFullstackSaaS.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullstackSaaS.Application.Features.UserAuth.Commands.Social_Login
+{
+    public class UserAuthSocialLoginCommand :IRequest<ResponseDto<JwtDto>>
+    {
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public UserAuthSocialLoginCommand(string email, string firstName,string lastName) 
+        {
+
+            Email = email;
+
+            FirstName = firstName;
+
+            LastName = lastName;        
+        }
+
+        public UserAuthSocialLoginCommand() 
+        {
+
+        }
+    }
+}
