980bda4d619ad1afeb017c9a04403adc103584c6
Beyza Hilal <beyzakaramert@gmail.com>
2024-07-01 14:34:54 +0300

    [Feature]GoogleObjectStorageManager

--- MextFullstackSaaS.Infrastructure/Services/GoogleObjectStorageManager.cs
+++ MextFullstackSaaS.Infrastructure/Services/GoogleObjectStorageManager.cs
@@ -1,17 +1,55 @@
-﻿using MextFullstackSaaS.Application.Common.Interfaces;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using MextFullstackSaaS.Application.Common.Interfaces;
 
 namespace MextFullstackSaaS.Infrastructure.Services
 {
     public class GoogleObjectStorageManager : IObjectStorageService
     {
-        public Task<string> UploadImageAsync(string imageData, CancellationToken cancellationToken)
+        private const string BucketName = "iconbuilderai-iconn-us";
+        private readonly GoogleCredential _credential;
+
+        public GoogleObjectStorageManager()
         {
-            throw new NotImplementedException();
+            _credential = GoogleCredential.FromJson("C:\\Users\\beyza\\Desktop\\phrasal-descent-428107-t8-62ee50c59f1e.json");
+        }
+        public async Task<string> UploadImageAsync(string imageData, CancellationToken cancellationToken)
+        {
+            // Convert the base64 string to byte array
+            var imageBytes = Convert.FromBase64String(imageData);
+
+            // Create a new MemoryStream
+            using var imageStream = new MemoryStream(imageBytes);
+
+            // Create a new Google Cloud Storage client
+            using var storage = await StorageClient.CreateAsync(_credential);
+
+            // Generate a unique filename
+            string fileName = $"{Guid.NewGuid()}.jpg";
+
+            // Upload the file to Google Cloud Storage
+            var uploadedObject = await storage.UploadObjectAsync(
+                BucketName,
+                fileName,
+                "image/jpeg",
+                imageStream,
+                cancellationToken: cancellationToken);
+
+            // Return the public URL of the uploaded image
+            //return $"https://storage.googleapis.com/{BucketName}/{fileName}";
+            return fileName;
+
         }
 
-        public Task<List<string>> UploadImageAsync(List<string> imagesData, CancellationToken cancellationToken)
+        public async Task<List<string>> UploadImagesAsync(List<string> imagesData, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var uploadTasks = imagesData.Select(imagesData => UploadImageAsync(imagesData, cancellationToken));
+
+            var results = await Task.WhenAll(uploadTasks);
+
+            return results.ToList();
+        }
+
         }
     }
-}
+
