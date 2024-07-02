9c5adb6cf83ad929b64cfaa59f395501064ddd2c
Beyza Hilal <beyzakaramert@gmail.com>
2024-07-02 11:01:00 +0300

    [Feature]add ResetPassword

--- MextFullstackSaaS.Application/Common/Interfaces/IIdentityService.cs
+++ MextFullstackSaaS.Application/Common/Interfaces/IIdentityService.cs
@@ -4,22 +4,25 @@ using MextFullstackSaaS.Application.Features.UserAuth.Commands.Register;
 using MextFullstackSaaS.Application.Features.UserAuth.Commands.Login;
 using MextFullstackSaaS.Application.Features.UserAuth.Commands.VerifyEmail;
 using MextFullstackSaaS.Application.Features.UserAuth.Commands.Social_Login;
+using MextFullstackSaaS.Application.Features.UserAuth.Commands.ResetPassword;
 
 namespace MextFullstackSaaS.Application.Common.Interfaces
 {
     public interface IIdentityService
     {
-        Task<UserAuthRegisterResponseDto> RegisterAsync(UserAuthRegisterCommand command, CancellationToken cancellationToken);
-        Task<JwtDto> LoginAsync(UserAuthLoginCommand command, CancellationToken cancellationToken);
-        Task<JwtDto> SocialLoginAsync(UserAuthSocialLoginCommand command, CancellationToken cancellationToken);
-        Task<bool>IsEmailExistsAsync (string email, CancellationToken cancellationToken);
-        Task<bool>CheckPasswordSignInAsync (string email, string password, CancellationToken cancellationToken);
-        Task<bool>VerifyEmailAsync(UserAuthVerifyEmailCommand command, CancellationToken cancellationToken);
-        Task<bool>CheckIfEmailVerifiedAsync(string email, CancellationToken cancellationToken);
-        Task<UserAuthResetPasswordResponseDto> ForgotPasswordAsync(string email, CancellationToken cancellationToken);
-        Task<bool> ResetPasswordAsync(UserAuthResetPasswordCommand command, CancellationToken cancellationToken);
-        Task<bool> ChangePasswordAsync(Guid userId, string currentPassword, string newPassword, CancellationToken cancellationToken);
+        Task<UserAuthRegisterResponseDto> RegisterAsync(UserAuthRegisterCommand userAuthRegisterCommand, CancellationToken cancellationToken);
+        Task<JwtDto> LoginAsync(UserAuthLoginCommand userAuthLoginCommand, CancellationToken cancellationToken);
+        Task<JwtDto> SocialLoginAsync(UserAuthSocialLoginCommand userAuthSocialLoginCommand, CancellationToken cancellationToken);
+        Task<bool> IsEmailExistsAsync(string email, CancellationToken cancellationToken);
+        Task<bool> CheckPasswordSignInAsync(string email, string password, CancellationToken cancellationToken);
+        Task<bool> VerifyEmailAsync(UserAuthVerifyEmailCommand command, CancellationToken cancellationToken);
+        Task<bool> CheckIfEmailVerifiedAsync(string email, CancellationToken cancellationToken);
 
+        Task<UserAuthResetPasswordResponseDto> ForgotPasswordAsync(string email, CancellationToken cancellationToken);
+
+        Task<bool> ResetPasswordAsync(UserAuthResetPasswordCommand command, CancellationToken cancellationToken);
+
+        Task<bool> ChangePasswordAsync(Guid userId, string currentPassword, string newPassword, CancellationToken cancellationToken);
 
     }
 }
---
+++ MextFullstackSaaS.Application/Features/UserAuth/Commands/ResetPassword/UserAuthResetPasswordCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using MextFullstackSaaS.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullstackSaaS.Application.Features.UserAuth.Commands.ResetPassword
+{
+    public class UserAuthResetPasswordCommand : IRequest<ResponseDto<bool>>
+    {
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
