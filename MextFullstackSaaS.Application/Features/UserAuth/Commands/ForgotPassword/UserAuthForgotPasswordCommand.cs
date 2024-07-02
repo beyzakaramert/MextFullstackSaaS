ddf47b808f13607db045fa39dc52bfbe50ab5949
Beyza Hilal <beyzakaramert@gmail.com>
2024-07-02 10:56:30 +0300

    [Feature]add ForgotPassword

---
+++ MextFullstackSaaS.Application/Features/UserAuth/Commands/ForgotPassword/UserAuthForgotPasswordCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using MextFullstackSaaS.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullstackSaaS.Application.Features.UserAuth.Commands.ForgotPassword
+{
+    public class UserAuthForgotPasswordCommand : IRequest<ResponseDto<bool>>
+    {
+        public string Email { get; set; }
+
+    }
+}
