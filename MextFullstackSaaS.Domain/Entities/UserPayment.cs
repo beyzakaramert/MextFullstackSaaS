65c1f7f9b1cb705277f033cc5dc5d29109132ef1
Beyza Hilal <beyzakaramert@gmail.com>
2024-07-05 10:03:42 +0300

    [Feature]add UserPayment

---
+++ MextFullstackSaaS.Domain/Entities/UserPayment.cs
@@ -0,0 +1,18 @@
+﻿using MextFullstackSaaS.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullstackSaaS.Domain.Entities
+{
+    public class UserPayment:EntityBase<Guid>
+    {
+        public string ConversationId { get; set; }
+
+        public string BasketId { get; set; }
+
+        public string Token { get; set;}
+    }
+}
