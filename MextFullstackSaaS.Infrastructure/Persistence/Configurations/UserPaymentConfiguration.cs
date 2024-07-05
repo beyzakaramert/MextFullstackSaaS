1dab279164707df6afd87920ce0228089c1687ab
Beyza Hilal <beyzakaramert@gmail.com>
2024-07-05 11:01:08 +0300

    [Feature]

---
+++ MextFullstackSaaS.Infrastructure/Persistence/Configurations/UserPaymentConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullstackSaaS.Infrastructure.Persistence.Configurations
+{
+    public class UserPaymentConfiguration : IEntityTypeBaseConvention
+    {
+    }
+}
