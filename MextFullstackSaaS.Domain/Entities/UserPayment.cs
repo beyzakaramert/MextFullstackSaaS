dba5d181877a76361408cd8e4d43c344e8ff4a80
Beyza Hilal <beyzakaramert@gmail.com>
2024-07-05 10:14:33 +0300

    [Feature]

--- MextFullstackSaaS.Domain/Entities/UserPayment.cs
+++ MextFullstackSaaS.Domain/Entities/UserPayment.cs
@@ -1,4 +1,5 @@
 ﻿using MextFullstackSaaS.Domain.Common;
+using MextFullstackSaaS.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,5 +17,10 @@ namespace MextFullstackSaaS.Domain.Entities
         public string Token { get; set;}
 
         public string Price { get; set; }
+
+        public string PaidPrice { get; set; }
+
+        public CurrencyCode CurrencyCode { get; set; }
+        public string Ip { get; set; }
     }
 }
